5047ef8c167ad095899f6047abfb2437825e3c08
Liam Chipman <chipman.liam@gmail.com>
2022-01-27 20:52:56 -0300

    First commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.helloworld}>
+      <Text>Hello World!</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  helloworld: {
+    width: 200,
+    height: 200,
+    backgroundColor: "#00ddfa",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
